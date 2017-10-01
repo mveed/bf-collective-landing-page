8545c5b9be3d55ed3ddbd6bb792fc285a11ed83d
mveed <mattveeder@gmail.com>
2017-09-30 19:13:30 -0600

    bf-collective landing page initial
    Bearfoot collective urban garden landing page start

---
+++ bearfoot_script.js
@@ -0,0 +1,15 @@
+$(document).ready( function() {
+
+
+
+$(".test").click( function() {
+  transitionTesting();
+});
+
+function transitionTesting() {
+  $("#2").toggleClass("transition-out");
+}
+
+
+
+}); // end of doc ready
